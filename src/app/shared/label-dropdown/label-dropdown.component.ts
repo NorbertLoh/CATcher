380a00ecd5bdde128a58c6623947cc7b5d8b9876
Loh Ze Qing Norbert <lohnorbert@gmail.com>
2025-01-14 18:35:46 +0800

    Add tooltips with shorter definitions to dropdowns

--- src/app/shared/label-dropdown/label-dropdown.component.ts
+++ src/app/shared/label-dropdown/label-dropdown.component.ts
@@ -7,6 +7,17 @@ import { LabelCategory, LabelService } from '../../core/services/label.service';
 export const WHITE_TEXT_CLASS = 'white-text';
 export const BLACK_TEXT_CLASS = 'black-text';
 
+const LABEL_DEFINITION_SHORT = {
+  VeryLow: 'A flaw that is purely cosmetic and does not affect usage',
+  Low: 'A flaw that is unlikely to affect normal operations of the product',
+  Medium: 'A flaw that causes occasional inconvenience to some users but they can continue to use the product',
+  High: 'A flaw that affects most users and causes major problems for users.i.e., makes the product almost unusable for most users.',
+  DocumentationBug: 'A flaw in the documentation e.g., a missing step, a wrong instruction, typos',
+  FeatureFlaw:
+    'Some functionality missing from a feature delivered in the current version in a way that the feature becomes less useful to the intended target user for normal usage. i.e., the feature is not complete',
+  FunctionalityBug: 'A functionality does not work as specified/expected'
+};
+
 @Component({
   selector: 'app-label-dropdown',
   templateUrl: './label-dropdown.component.html',
@@ -41,7 +52,12 @@ export class LabelDropdownComponent implements OnInit {
   }
 
   hasLabelDefinition(label: Label): boolean {
+    console.log(label);
     return this.labelService.getLabelDefinition(label.labelValue, label.labelCategory) !== null;
+  }
+
+  getLabelDefinition(label: Label): string {
+    return LABEL_DEFINITION_SHORT[label.labelValue];
   }
 
   get dropdownTextColor(): string {
